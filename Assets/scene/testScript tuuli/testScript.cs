6033b4fc3a71993da76130a1ab9d1106ca755e91
Tuuli Saarinen <saari.tuulinen@gmail.com>
2014-08-08 09:48:27 -0400

    UI layouts in NGUI, in scene "main tuuli"

---
+++ Assets/scene/testScript tuuli/testScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+
+
+	public UIButton btn1;
+	public UIButton btn2;
+	public UIButton btn3;
+	public UIButton btn4;
+	public UIButton btn5;
+	public UIButton btn6;
+
+	bool btn1Pressed;
+	bool btn2Pressed;
+	bool btn3Pressed;
+	bool btn4Pressed;
+	bool btn5Pressed;
+	bool btn6Pressed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		btn1Pressed = false;
+		btn2Pressed = false;
+		btn3Pressed = false;
+		btn4Pressed = false;
+		btn5Pressed = false;
+		btn6Pressed = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PressedButton1(){
+	}
+
+	public void PressedButton2(){
+	}
+
+	public void PressedButton3(){	
+	}
+
+	public void PressedButton4(){	
+	}
+
+	public void PressedButton5(){	
+	}
+
+	public void PressedButton6(){	
+	}
+
+}
